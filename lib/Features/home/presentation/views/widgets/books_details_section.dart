2cd01dfd2a462d73b46766361ed5aa2d092156b3
MostafaSoubi7 <mm0334471@gmail.com>
2024-11-29 21:56:02 +0200

    display featured books list view part 2

--- lib/Features/home/presentation/views/widgets/books_details_section.dart
+++ lib/Features/home/presentation/views/widgets/books_details_section.dart
@@ -14,7 +14,9 @@ class BookDetailsSection extends StatelessWidget {
       children: [
         Padding(
           padding: EdgeInsets.symmetric(horizontal: width * .2),
-          child: const CustomBookImage(),
+          child: const CustomBookImage(
+            imageUrl: "https://unsplash.it/200/120",
+          ),
         ),
         const SizedBox(
           height: 43,
--- lib/Features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,23 +1,22 @@
-import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
-  const CustomBookImage({super.key});
+  const CustomBookImage({super.key, required this.imageUrl});
+  final String imageUrl;
 
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2.6 / 4,
       child: Container(
-        width: 100,
-        height: MediaQuery.of(context).size.height * .25,
+        // width: 100,
+        // height: MediaQuery.of(context).size.height * .25,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
-          color: Colors.red,
-          image: const DecorationImage(
+          image: DecorationImage(
             fit: BoxFit.fill,
-            image: AssetImage(
-              AssetsData.tetsImages,
+            image: NetworkImage(
+              imageUrl,
             ),
           ),
         ),
