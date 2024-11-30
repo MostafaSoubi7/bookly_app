72de7502b8cf144d5c9d0628ffb511a62ebbc14f
MostafaSoubi7 <mm0334471@gmail.com>
2024-11-30 21:44:22 +0200

    fetch Similar Books

--- lib/Features/home/data/models/book_model/repos/home_repo_implement.dart
+++ lib/Features/home/data/models/book_model/repos/home_repo_implement.dart
@@ -14,17 +14,22 @@ class HomeRepoImplement implements HomeRepo {
     try {
       var data = await apiService.get(
           endPoint:
-              "volumes?q=subject:Programming&Filtering=free-ebooks&Sorting=newest");
+              "volumes?q=computer science&Filtering=free-ebooks&Sorting=newest");
 
       List<BookModel> books = [];
       for (var item in data['items']) {
-        books.add(
-          BookModel.fromJson(item),
-        );
+        try {
+          books.add(
+            BookModel.fromJson(item),
+          );
+        } catch (e) {
+          books.add(
+            BookModel.fromJson(item),
+          );
+        }
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure(e.toString()));
       if (e is DioException) {
         return left(ServerFailure.fromDioError(e));
       }
@@ -41,6 +46,34 @@ class HomeRepoImplement implements HomeRepo {
     try {
       var data = await apiService.get(
           endPoint: "volumes?q=subject:Programming&Filtering=free-ebooks");
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(
+          BookModel.fromJson(item),
+        );
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks({
+    required String category,
+  }) async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              "volumes?q=computer science&Filtering=free-ebooks&Sorting=relevance");
 
       List<BookModel> books = [];
       for (var item in data['items']) {
