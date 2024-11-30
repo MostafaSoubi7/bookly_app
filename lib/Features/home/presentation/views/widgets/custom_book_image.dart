cb5e4683c8cb9cc860c2e6159431840cabf9128d
MostafaSoubi7 <mm0334471@gmail.com>
2024-11-30 17:45:07 +0200

    cached network image

--- lib/Features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
@@ -6,9 +7,22 @@ class CustomBookImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          imageUrl: imageUrl,
+          fit: BoxFit.fill,
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+        ),
+      ),
+    );
+  }
+}
+
+/*
+Container(
         // width: 100,
         // height: MediaQuery.of(context).size.height * .25,
         decoration: BoxDecoration(
@@ -20,7 +34,4 @@ class CustomBookImage extends StatelessWidget {
             ),
           ),
         ),
-      ),
-    );
-  }
-}
+      ), */
